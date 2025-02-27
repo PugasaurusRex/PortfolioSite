6d2622d35d44f340c513804cc60a3d7397701e7d
Mark Ellis <markde717@gmail.com>
2025-02-27 15:24:34 -0500

    Add current position to references

--- scripts/references.js
+++ scripts/references.js
@@ -1,4 +1,4 @@
-function createReference(name, relationship, phone, email) {
+function createReference(name, relationship, currentPosition, phone, email) {
   const references = document.querySelector('.references-container');
   const html = `
     <div class="reference">
@@ -6,6 +6,10 @@ function createReference(name, relationship, phone, email) {
       <div class="reference-info-row">
         <p class="reference-title">Relationship:</p>
         <p class="reference-info">${relationship}</p>
+      </div>
+      <div class="reference-info-row">
+        <p class="reference-title">Current Position:</p>
+        <p class="reference-info">${currentPosition}</p>
       </div>
       <div class="reference-info-row">
         <p class="reference-title">Phone:</p>
@@ -21,6 +25,6 @@ function createReference(name, relationship, phone, email) {
   references.innerHTML += html;
 }
 
-createReference('Doga Demirel', 'Manager at Florida Polytechnic University', '(405) 325-4042', 'doga@ou.edu');
-createReference('Joseph Patullo', 'Manager at Avra Medical Robotics', '(954) 300-9321', 'joseph.patullo@gmail.com');
-createReference('Alex Adams', 'Friend and Previous Roomate', '(863) 224-1450', 'alexanderandrewadams1@gmail.com');
+createReference('Doga Demirel', 'Manager at Florida Polytechnic University', 'Associate Professor at University of Oklahoma', '(405) 325-4042', 'doga@ou.edu');
+createReference('Joseph Patullo', 'Manager at Avra Medical Robotics', 'Senior Systems Software Engineer at NVIDIA', '(954) 300-9321', 'joseph.patullo@gmail.com');
+createReference('Alex Adams', 'Friend and Previous Roomate', 'Municipal Low-Income Housing Asset Management at Citi', '(863) 224-1450', 'alexanderandrewadams1@gmail.com');

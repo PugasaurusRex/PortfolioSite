5bbef9fdade16992959b3a6ab282fba62056564b
Mark Ellis <markde717@gmail.com>
2025-02-28 12:55:41 -0500

    Folder for html files. Testing embedded game

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 function createNavBar() {
   const nav = document.querySelector('.navbar');
   const html = `
-      <a class="nav-link" href="index.html">Home</a>
+      <a class="nav-link" href="../index.html">Home</a>
       <a class="nav-link" href="experience.html">Experience</a>
       <a class="nav-link" href="projects.html">Projects</a>
       <a class="nav-link" href="references.html">References</a>

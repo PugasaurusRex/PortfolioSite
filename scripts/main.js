988e1c4800d0453c628982ba3d7b4f10c9b7a5c6
Mark Ellis <markde717@gmail.com>
2025-02-28 11:58:40 -0500

    Remove contact section.

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,6 @@ function createNavBar() {
       <a class="nav-link" href="index.html">Home</a>
       <a class="nav-link" href="experience.html">Experience</a>
       <a class="nav-link" href="projects.html">Projects</a>
-      <a class="nav-link" href="contact.html">Contact</a>
       <a class="nav-link" href="references.html">References</a>
   `;
 
--- scripts/references.js
+++ scripts/references.js
@@ -27,4 +27,4 @@ function createReference(name, relationship, currentPosition, phone, email) {
 
 createReference('Doga Demirel', 'Manager at Florida Polytechnic University', 'Associate Professor at University of Oklahoma', '(405) 325-4042', 'doga@ou.edu');
 createReference('Joseph Patullo', 'Manager at Avra Medical Robotics', 'Senior Systems Software Engineer at NVIDIA', '(954) 300-9321', 'joseph.patullo@gmail.com');
-createReference('Alex Adams', 'Friend and Previous Roomate', 'Municipal Low-Income Housing Asset Management at Citi', '(863) 224-1450', 'alexanderandrewadams1@gmail.com');
+createReference('Alex Adams', 'Friend', 'Municipal Low-Income Housing Asset Management at Citi', '(863) 224-1450', 'alexanderandrewadams1@gmail.com');

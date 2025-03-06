8acf432b593c1223314da2fac053a634f7e8a2b7
Mark Ellis <markde717@gmail.com>
2025-03-06 15:41:16 -0500

    Create boxes for projects page

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,3 +1,21 @@
 import { createNavBar } from "./navbar.js";
 
 createNavBar('../');
+
+const projectGrid = document.querySelector('.project-grid');
+
+function createProject(name) {
+  projectGrid.innerHTML += `
+    <div class="project-box">
+      <h3>${name}</h3>
+    </div>
+  `;
+}
+
+createProject('Project');
+createProject('Project');
+createProject('Project');
+createProject('Project');
+createProject('Project');
+createProject('Project');
+createProject('Project');
